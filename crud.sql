bcde412ab6b70c1135c0ccf5ba4384768294a0c4
Anav Chand <anavkki@gmail.com>
2024-11-24 22:03:57 +1200

    feat: add crud

---
+++ crud.sql
@@ -0,0 +1,29 @@
+-- Create
+
+-- INSERT INTO Artist(ArtistId, Name)
+
+-- VALUES (10000, 'anav')
+
+-- Read
+
+-- SELECT Name AS "Artiest Name"
+
+-- FROM Artist
+
+-- WHERE ArtistId = 900
+
+-- Update
+
+-- UPDATE Artist
+
+-- SET Name = 'Anav'
+
+-- WHERE ArtistId = 900
+
+-- Delete
+
+DELETE FROM Artist
+
+WHERE ArtistId = 900
+
+
